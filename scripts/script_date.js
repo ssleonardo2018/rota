2f07d7a6957e489f9b5b724ef3a1fa90904dfe0b
ssleonardo2018 <115373834+ssleonardo2018@users.noreply.github.com>
2025-04-10 11:23:04 -0300

    Add files via upload
    Criado Aplicação Web Rota

---
+++ scripts/script_date.js
@@ -0,0 +1,6 @@
+function exibirData() {
+    var data = document.getElementById('data').value;
+    document.getElementById('dataSelecionada').textContent = data ? data : 'Nenhuma data selecionada';
+}
+
+        
